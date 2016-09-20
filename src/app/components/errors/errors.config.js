e9836117e8a1d8d7fdb1d86c5f39dd4b00bba0e2
AlexandrDrakon <guselnikov1988@ngs.ru>
2016-09-21 00:46:13 +0600

    edit errors.config.js

--- src/app/components/errors/errors.config.js
+++ src/app/components/errors/errors.config.js
@@ -8,19 +8,23 @@
   /** ngInject */
   function httpProviderConfig($httpProvider) {
 
-    $httpProvider.interceptors.push(function() {
-      return {
-        'responseError': function(rejection) {
-          if (rejection.status === 400) {
-            $state.go('400');
-          } else if (rejection.status === 404) {
-            $state.go('404');
-          } else if (rejection.status === 500) {
-            $state.go('500');
+    $httpProvider.interceptors.push(
+      /** ngInject */
+      function($injector, $q) {
+        return {
+          'responseError': function(rejection) {
+            if (rejection.status === 400) {
+              $injector.get('$state').go('400');
+            } else if (rejection.status === 404) {
+              $injector.get('$state').go('404');
+            } else if (rejection.status === 500) {
+              $injector.get('$state').go('500');
+            }
+            return $q.reject(rejection);
           }
         }
       }
-    });
+    );
 
   }
 
